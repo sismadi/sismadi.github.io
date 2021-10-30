b49ec6b4b5162e1e6e41abd81ca394f9da779db6
wawan sismadi <wawan@sismadi.com>
2021-10-30 11:49:51 +0700

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,24 @@
+modal={
+gebi:function(i){ return  document.getElementById(i);},
+data:'<hr class="image"><hr><hr><hr>',
+view:function() {
+out='<div id="myModal" class="modal">\
+<div class="modal-content">\
+<span class="close">&times;</span>';
+out+=this.data;
+out+='</div></div>';
+this.gebi('modal').innerHTML=out;
+this.open('modal');
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { modal.close('modal')}
+},
+
+close:function(id){ var z=this.gebi(id);
+z.className = z.className.replace('show', 'hide');
+},
+
+open:function(id){ var z=this.gebi(id);
+z.className = z.className.replace('hide', 'show');
+},
+
+};
